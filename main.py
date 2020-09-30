01cff499179585e0bc27ba328712e6fd6cfe9f86
Mr. Sp4rX <71897067+MrSp4rX@users.noreply.github.com>
2020-09-30 20:37:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+from plyer import notification
+import requests
+import re
+from time import sleep
+import os
+
+url = 'http://newsapi.org/v2/top-headlines?country=in&apiKey=11444a78f247489a881fe41fa6a39c16'
+
+response = requests.get(url)
+raw_text = response.json()
+
+# print(raw_text)
+# print(raw_text['totalResults'])
+no = 1
+for i in range(10):
+	if raw_text['status'] == "ok":
+		print("+++++++++++++++++++++++ News No. "+str(no)+" +++++++++++++++++++++++")
+		articles = raw_text['articles']
+		article = articles[0]
+		source = article['source']
+		# print(article.keys())
+		if str(source) == "None":
+			print("The Source of this news is Unknown.")
+		else:
+			print("The Source of this news is "+str(source['name'])+".")
+		author = str(article['author'])
+		if author == "None":
+			print("The Author of this News is Unknown.")
+		else:
+			print("The Author of this News is "+str(article['author'])+".")
+		title = article['title']
+		print("The title of this News is ", end="")
+		a = 0
+		for i in range(len(title)):
+			t =  title[a]
+			if t==":":
+				break
+			print(t, end="")
+			a = a+1
+		print(".\n\nDescription:")
+		print(str(article['description'])+".\n")
+		con = article['content']
+		con = re.compile(r'<[^>]+>').sub('', con)
+		con = con.replace('"', '')
+		print("Content:")
+		print(con)
+		time = article['publishedAt']
+		print("This Article was Published on "+str(time[0:9]))
+		print('\n\n')
+		pwd = os.getcwd()
+		des = str(article['description'])
+		notification.notify(
+			title = str(no)+'. Top News',
+			message = des[0:225],
+			app_icon = os.getcwd()+"\\news.ico",
+			timeout = 5
+		)
+		sleep(6)
+		no = no + 1
+	else:
+		print("Please Check Your Internet Connection!!!")
+		exit()
